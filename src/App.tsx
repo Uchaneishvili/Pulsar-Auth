a97017832857de4d61aa0209a8db96ed2d7891c9
Giga Uchaneishvili <g.uchaneishvili@tnet.loc>
2024-05-19 17:48:34 +0400

    markup is completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import Login from "./pages/login/Login";
 
 function App() {
-	return <div></div>;
+	return (
+		<div>
+			<Login />
+		</div>
+	);
 }
 
 export default App;
---
+++ src/pages/login/Login.tsx
@@ -0,0 +1,54 @@
+import {
+	EmailIcon,
+	PasswordIcon,
+	ShowPasswordIcon,
+} from "../../components/ui/icon";
+import styles from "./Login.module.css";
+const Login = () => {
+	return (
+		<>
+			<div className={styles.container}>
+				<form className={styles.form}>
+					<div className={styles.flexColumn}>
+						<label>Email </label>
+					</div>
+					<div className={styles.inputForm}>
+						<EmailIcon />
+						<input
+							type="text"
+							className={styles.input}
+							placeholder="Enter your Email"
+						/>
+					</div>
+
+					<div className={styles.flexColumn}>
+						<label>Password </label>
+					</div>
+					<div className={styles.inputForm}>
+						<PasswordIcon />
+						<input
+							type="password"
+							className={styles.input}
+							placeholder="Enter your Password"
+						/>
+						<ShowPasswordIcon />
+					</div>
+
+					<div className={styles.flexRow}>
+						<div>
+							<input type="checkbox" />
+							<label>Remember me </label>
+						</div>
+						<span className={styles.span}>Forgot password?</span>
+					</div>
+					<button className={styles.buttonSubmit}>Sign In</button>
+					<p className={styles.p}>
+						Don't have an account? <span className={styles.span}>Sign Up</span>
+					</p>
+				</form>
+			</div>
+		</>
+	);
+};
+
+export default Login;
